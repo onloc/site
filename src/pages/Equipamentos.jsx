d0ce66e49745bc9c40d213da95f3c16f951e8734
lucasmarcosds <lukeskaiw@gmail.com>
2026-03-10 08:50:47 -0300

    ADD Routes

---
+++ src/pages/Equipamentos.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export function Equipamentos() {
+  return (
+    <div>
+      <h1>Equipamentos</h1>
+      <Link to="/">Voltar para a Home</Link>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/equipamentos">
+        <button>Ir para Equipamentos</button>
+      </Link>
+    </div>
+  );
+}
