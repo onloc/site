c1e6d73a06db5f085270fc259f688b33fadc0064
Taizi0 <166618351+Taizi0@users.noreply.github.com>
2026-03-10 10:23:05 -0300

    more header structure

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,37 @@
 import logo from "../../assets/logo-branca.png";
-import { Wrapper, Container, Logo, Nav, NavItem, Actions, Button } from "./styles";
+import Search from "../../assets/icone-lupa.png";
+import {
+  Wrapper,
+  Container,
+  Logo,
+  Nav,
+  NavItem,
+  Actions,
+  Button,
+  DropdownMenu,
+  Dropdown,
+  DropdownItem,
+} from "./styles";
 
 function Header() {
   return (
     <Wrapper>
       <Container>
-
         <Logo src={logo} alt="logo" />
 
         <Nav>
           <NavItem>Home</NavItem>
-          <NavItem>Equipamentos</NavItem>
+          <Dropdown>
+            <NavItem>Equipamentos ▾</NavItem>
+
+            <DropdownMenu>
+              <DropdownItem>Desktop</DropdownItem>
+              <DropdownItem>Minidesks</DropdownItem>
+              <DropdownItem>Notebooks</DropdownItem>
+              <DropdownItem>Servidores</DropdownItem>
+              <DropdownItem>Workstation</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
           <NavItem>Quem Somos</NavItem>
           <NavItem>Soluções</NavItem>
           <NavItem>Suporte</NavItem>
@@ -19,9 +40,9 @@ function Header() {
         </Nav>
 
         <Actions>
+          <img src={Search} alt="buscar" />
           <Button>Quero alugar</Button>
         </Actions>
-
       </Container>
     </Wrapper>
   );
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.header`
   width: 100%;
-  background-color: #333;
+  background-color: #000000;
 `;
 
 export const Container = styled.div`
@@ -19,12 +19,12 @@ export const Container = styled.div`
 `;
 
 export const Logo = styled.img`
-  height: 40px;
+  height: 30px;
 `;
 
 export const Nav = styled.nav`
   display: flex;
-  gap: 25px;
+  gap: 32px;
 `;
 
 export const NavItem = styled.a`
@@ -41,7 +41,44 @@ export const NavItem = styled.a`
 export const Actions = styled.div`
   display: flex;
   align-items: center;
-  gap: 20px;
+  gap: 25px;
+`;
+
+export const Dropdown = styled.div`
+  position: relative;
+
+  &:hover div {
+    display: flex;
+  }
+`;
+
+export const DropdownMenu = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 30px;
+
+  background: white;
+  border-radius: 6px;
+
+  display: none;
+  flex-direction: column;
+
+  box-shadow: 0 5px 15px rgba(0,0,0,0.15);
+`;
+
+export const DropdownItem = styled.a`
+  padding: 10px 20px;
+  cursor: pointer;
+  color: #333;
+  text-decoration: none;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid #DAE1E5;
+  }
+
+  &:hover {
+    background: #f5f5f5;
+  }
 `;
 
 export const Button = styled.button`
