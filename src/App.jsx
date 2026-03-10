f4f36d5ec3ce94361fc98e48e9c6c939a4253487
lucasmarcosds <lukeskaiw@gmail.com>
2026-03-10 08:51:45 -0300

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Equipamentos } from './pages/Equipamentos'; // Importe a página
+import { Equipamentos } from './pages/Equipamentos';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/equipamentos" element={<Equipamentos />} /> {/* Defina a rota */}
+        <Route path="/equipamentos" element={<Equipamentos />} />
       </Routes>
     </BrowserRouter>
   );
