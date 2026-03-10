c98e9515b736453b59603a94ba710ae8f1a162a0
lucasmarcosds <lukeskaiw@gmail.com>
2026-03-10 10:40:19 -0300

    ADD Favicon and title base

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,27 +1,33 @@
-import { FooterContainer, SectionTitle, FooterLink } from "./styles";
+import {
+  FooterContainer,
+  SectionTitle,
+  FooterLink,
+  LogoImage,
+  FooterCopyright,
+} from "./styles";
+import logo from "../../assets/logo-branca.png";
 
 const Footer = () => (
   <FooterContainer>
     <div>
-      <img src="logo.png" alt="On.Loc" />
+      <LogoImage src={logo} alt="On.Loc" />
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
     </div>
-
     <div>
       <SectionTitle>Institucional</SectionTitle>
       <FooterLink href="#">Home</FooterLink>
       <FooterLink href="#">Equipamentos</FooterLink>
     </div>
-
     <div>
       <SectionTitle>Contato</SectionTitle>
       <p>Comercial: (19) 3535-9100</p>
       <p>E-mail: contato@onloc.com.br</p>
     </div>
-
     <div>
       <SectionTitle>Rede Social</SectionTitle>
     </div>
+    <FooterCopyright/>
+    2025 - Onloc. Todos os direitos reservados. Desenovolvido por Postali
   </FooterContainer>
 );
 
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,17 +1,14 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
-  background-color: #1a1a1a;
+  background-color: #1e1e20;
   color: #ffffff;
   padding: 60px 10%;
-
   width: 100%;
   box-sizing: border-box;
-
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 40px;
-
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
@@ -33,4 +30,19 @@ export const FooterLink = styled.a`
   &:hover {
     color: #ffffff;
   }
+`;
+
+export const LogoImage = styled.img`
+  width: 208px;
+  height: 39px;
+  object-fit: contain;
+`;
+
+export const FooterCopyright = styled.footer`
+  background-color: #4c5258;
+  width: 1920;
+  height: 68;
+  top: 480px;
+  angle: 0 deg;
+  opacity: 1;
 `;
