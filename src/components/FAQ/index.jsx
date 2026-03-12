2d78f6543d40b8334414081feb7b18f946d4a2a9
lucasmarcosds <lukeskaiw@gmail.com>
2026-03-12 13:36:18 -0300

    faqData to prop, now is a component

--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -9,49 +9,7 @@ import {
 
 import Arrow from "../../assets/arrow_insert.png";
 
-const faqData = [
-  {
-    id: "01",
-    title: "Como funciona?",
-    content: `Para contratar as soluções personalizadas ON.LOC, basta ter em mente a quantidade e a configuração desejada, ou simplesmente nos informar a funcionalidade esperada com a máquina.
-
-Com essas informações, indicaremos a melhor configuração, levando em conta a disponibilidade do estoque da ONLOC.
-
-Os desktops e notebooks que fornecemos já vão com o sistema operacional original da Microsoft instalado, o Windows licenciado.
-
-A equipe de TI da sua preferência poderá instalar ou formatar a máquina com os softwares necessários, incluindo o pacote Office. Com isso, você estará à vontade para gerenciar seus sistemas usuais.`,
-  },
-  {
-    id: "02",
-    title: "E os aparelhos que não tem mais atualização de software?",
-    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
-    
-Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
-  },
-  {
-    id: "03",
-    title: "Como é o processo de análise?",
-    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
-    
-Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
-  },
-  {
-    id: "04",
-    title: "Quais formas de pagamento?",
-    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
-    
-Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
-  },
-  {
-    id: "05",
-    title: "O que acontece com o final da minha assinatura?",
-    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
-    
-Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
-  },
-];
-
-const FAQ = () => {
+const FAQ = ({data}) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const toggleAccordion = (index) => {
@@ -62,7 +20,7 @@ const FAQ = () => {
     <FAQContainer>
       <Title>Perguntas Frequentes:</Title>
 
-      {faqData.map((item, index) => (
+      {data.map((item, index) => (
         <AccordionItem key={item.id}>
           <AccordionHeader
             isOpen={activeIndex === index}
--- src/pages/EquipamentoDetalhes.jsx
+++ src/pages/EquipamentoDetalhes.jsx
@@ -11,6 +11,48 @@ export function EquipamentoDetalhes() {
   const location = useLocation();
   const { item } = location.state || {};
 
+  const faqData = [
+  {
+    id: "01",
+    title: "Como funciona?",
+    content: `Para contratar as soluções personalizadas ON.LOC, basta ter em mente a quantidade e a configuração desejada, ou simplesmente nos informar a funcionalidade esperada com a máquina.
+
+Com essas informações, indicaremos a melhor configuração, levando em conta a disponibilidade do estoque da ONLOC.
+
+Os desktops e notebooks que fornecemos já vão com o sistema operacional original da Microsoft instalado, o Windows licenciado.
+
+A equipe de TI da sua preferência poderá instalar ou formatar a máquina com os softwares necessários, incluindo o pacote Office. Com isso, você estará à vontade para gerenciar seus sistemas usuais.`,
+  },
+  {
+    id: "02",
+    title: "E os aparelhos que não tem mais atualização de software?",
+    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
+    
+Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
+  },
+  {
+    id: "03",
+    title: "Como é o processo de análise?",
+    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
+    
+Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
+  },
+  {
+    id: "04",
+    title: "Quais formas de pagamento?",
+    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
+    
+Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
+  },
+  {
+    id: "05",
+    title: "O que acontece com o final da minha assinatura?",
+    content: `Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi pellentesque. 
+    
+Donec sed nunc sed amet. Nunc sed nunc sed amet. Leo donec netus libero facilisi pellentesque. Donec sed nunc sed amet. Nunc sed nunc sed amet.`,
+  },
+];
+
   if (!item) {
     return (
       <>
@@ -31,7 +73,7 @@ export function EquipamentoDetalhes() {
       </div>
       <div style={{ background: "#fff", padding: "100px 200px " }}>
         <EquipamentoDetalhesInfo />
-        <FAQ />
+        <FAQ data={faqData}/>
       </div>
       <div style={{ background: "#EFF2F3", padding: "100px 100px " }}>
         <RecomendItems />
