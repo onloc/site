d0a178fcb4ad6339fbf7ea6e18615dd4850d4bf3
lucasmarcosds <lukeskaiw@gmail.com>
2026-03-14 12:16:06 -0300

    remove coment

--- src/pages/Suporte.jsx
+++ src/pages/Suporte.jsx
@@ -3,7 +3,6 @@ import BlackHeader from "../components/BlackHeader";
 import HeaderBanner from "../components/HeaderBanner";
 import FormContainer from "../components/FormContainer";
 import Typography from "../components/Typography";
-import ContentSlider from "../components/ContentSlider";
 
 import bgImage from "../assets/banner-solucoes.jpg";
 import softwareSuporte from "../assets/software-suporte.png";
@@ -11,76 +10,9 @@ import garantiaSuporte from "../assets/background-suporte.png";
 import relatoTraumec from "../assets/background-depoimento.png";
 import experiencia13Anos from "../assets/background-experience.png";
 
-// import image1 from "../assets/content-image-1.png";
-// import image2 from "../assets/content-image-2.png";
-// import image3 from "../assets/content-image-3.png";
-// import image4 from "../assets/content-image-4.png";
 import CardTextImage from "../components/CardTextImage";
 
 export function Suporte() {
-  // const cards = [
-  //   {
-  //     title: "Tecnologia para Empresas",
-  //     description:
-  //       "Alugar ou comprar? Descubra o que vale mais a pena para sua empresa em 2025",
-  //     date: "Out 30, 2024",
-  //     author: "Equipe Onloc",
-  //     comments: ["a", "b", "c", "d"],
-  //     image: image1,
-  //     category: "Tecnologia",
-  //   },
-  //   {
-  //     title: "Gestão de Equipamentos",
-  //     description:
-  //       "Como a locação reduz custos com TI e melhora a produtividade",
-  //     date: "Out 30, 2024",
-  //     author: "Equipe Onloc",
-  //     comments: ["a", "b", "c"],
-  //     image: image2,
-  //     category: "Tecnologia",
-  //   },
-  //   {
-  //     title: "Assistência Técnica",
-  //     description:
-  //       "5 sinais de que seus equipamentos precisam de manutenção preventiva",
-  //     date: "Out 30, 2024",
-  //     author: "Equipe Onloc",
-  //     comments: ["a", "b"],
-  //     image: image3,
-  //     category: "Tecnologia",
-  //   },
-  //   {
-  //     title: "Eficiência Operacional",
-  //     description:
-  //       "Por que empresas de logística e call center estão adotando locação em larga escala",
-  //     date: "Out 30, 2024",
-  //     author: "Equipe Onloc",
-  //     comments: ["a", "b", "c", "d", "e"],
-  //     image: image4,
-  //     category: "Tecnologia",
-  //   },
-
-  //   {
-  //     title: "Eficiência Operacional",
-  //     description:
-  //       "Por que empresas de logística e call center estão adotando locação em larga escala",
-  //     date: "Out 30, 2024",
-  //     author: "Equipe Onloc",
-  //     comments: ["a", "b", "c", "d", "e"],
-  //     image: image4,
-  //     category: "Tecnologia",
-  //   },
-  //   {
-  //     title: "Eficiência Operacional",
-  //     description:
-  //       "Por que empresas de logística e call center estão adotando locação em larga escala",
-  //     date: "Out 30, 2024",
-  //     author: "Equipe Onloc",
-  //     comments: ["a", "b", "c", "d", "e"],
-  //     image: image4,
-  //     category: "Tecnologia",
-  //   },
-  // ];
   return (
     <>
       <BlackHeader />
@@ -161,44 +93,6 @@ export function Suporte() {
       >
         <img src={softwareSuporte} alt="Software de Suporte" />
       </div>
-
-      {/* <div
-        style={{
-          margin: "120px auto 40px auto",
-          width: "622px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "16px",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <Typography
-          colorText="#1E1E20"
-          variant="Quicksand"
-          fontSize="40px"
-          fontWeight="500"
-        >
-          Nossos conteúdos
-        </Typography>
-        <Typography
-          variant="Montserrat"
-          colorText="#0E1017"
-          fontSize="16px"
-          fontWeight="400"
-        >
-          Em nosso blog você encontra notícias, dicas e artigos sobre assuntos
-          ligados a tecnologia de modo geral.
-        </Typography>
-      </div>
-
-      <div
-        style={{
-          margin: "120px auto 40px auto",
-        }}
-      >
-        <ContentSlider data={cards} />
-      </div> */}
 
       <FormContainer />
       <Footer />
