259a0fa7818d408751b56f9808ea3f5f61ef645b
Taizi0 <166618351+Taizi0@users.noreply.github.com>
2026-03-21 09:14:08 -0300

    change WhyLease text

--- src/components/WhyLease/index.jsx
+++ src/components/WhyLease/index.jsx
@@ -17,20 +17,17 @@ function WhyLeaseComponent() {
     <>
       <WhyLease $backgroundImage={backgroundImage}>
         <WhyLeaseContainer>
-        <WhyLeaseTitleContainer>
-          <WhyLeaseIcon src={computerIcon} alt="Computer Icon" />
-          <WhyLeaseTitle>Por que Locar?</WhyLeaseTitle>
-        </WhyLeaseTitleContainer>
-        <WhyLeaseText>
-          Alocar computadores com a Onloc é sinônimo de economia, flexibilidade
-          e tecnologia de ponta. Com soluções personalizadas e equipamentos
-          atualizados, sua empresa ganha em produtividade sem precisar investir
-          em infraestrutura.{" "}
-          <b>
-            Ideal para projetos temporários, expansão rápida ou modernização.
-          </b>
-        </WhyLeaseText>
-        <WhyLeaseImage src={whiteDesk} alt="White Desk" />
+          <WhyLeaseTitleContainer>
+            <WhyLeaseIcon src={computerIcon} alt="Computer Icon" />
+            <WhyLeaseTitle>Por que Locar?</WhyLeaseTitle>
+          </WhyLeaseTitleContainer>
+          <WhyLeaseText>
+            Alugar computadores é a forma mais prática de manter sua empresa
+            produtiva, com equipamentos atualizados, suporte especializado e
+            custos previsíveis.
+           <b> A locação elimina altos investimentos iniciais, reduz a preocupação com manutenção e permite que sua equipe foque no que realmente importa</b>
+          </WhyLeaseText>
+          <WhyLeaseImage src={whiteDesk} alt="White Desk" />
         </WhyLeaseContainer>
       </WhyLease>
     </>
