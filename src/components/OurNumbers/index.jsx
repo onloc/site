5d30745a3bd11f7b8738587a8ea5d66d1de70bab
ThalesAltarugio <166618351+Taizi0@users.noreply.github.com>
2026-04-08 09:14:28 -0300

    adjust ourNumbers numbers

--- src/components/OurNumbers/index.jsx
+++ src/components/OurNumbers/index.jsx
@@ -16,7 +16,7 @@ function OurNumbers() {
         <ContainerContent>
           <Title>Nossos Números</Title>
           <Subtitle>
-            Anos de experiência entregando soluções em tecnologia para empresas
+            Com mais de 13 anos de experiência entregando soluções em tecnologia para empresas
             de Rio Claro e região. Resultados construídos com parceria,
             confiança e infraestrutura de TI bem dimensionada. É assim que
             ajudamos negócios a crescer com tecnologia eficiente e sem
@@ -31,17 +31,17 @@ function OurNumbers() {
           </BlockSection>
 
           <BlockSection>
-            <Number>+480</Number>
+            <Number>+650</Number>
             <Text>Clientes atendidos</Text>
           </BlockSection>
 
           <BlockSection>
-            <Number>+6500</Number>
+            <Number>+8000</Number>
             <Text>Produtos locados</Text>
           </BlockSection>
 
           <BlockSection>
-            <Number>+4824</Number>
+            <Number>+4000</Number>
             <Text>Computadores locados</Text>
           </BlockSection>
         </Block>
