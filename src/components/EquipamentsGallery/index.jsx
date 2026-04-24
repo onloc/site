f460566f727b32e9bb2a9058987818b1d028a1f5
ThalesAltarugio <166618351+Taizi0@users.noreply.github.com>
2026-04-24 15:59:32 -0300

    change equipamentsGallery images

--- src/components/EquipamentsGallery/index.jsx
+++ src/components/EquipamentsGallery/index.jsx
@@ -5,7 +5,10 @@ import monitors from "../../assets/Gallery/screens.png";
 import notebooks from "../../assets/Gallery/notebooks.png";
 import servers from "../../assets/Gallery/servers.png";
 
-import desk from "../../assets/mock-produto.png";
+import work from "../../assets/equipamentos-2026/WORKSTATION HP Z2 G4 TOWER XEON E-2144G.webp";
+import screen from "../../assets/equipamentos-2026/1200.webp";
+import note from "../../assets/equipamentos-2026/NOTEBOOK WORKSTATION HP ZBOOK FURY 15 G7 I7-10850H 1.webp"
+import server from "../../assets/equipamentos-2026/SERVIDOR DELL POWEREDGE T440 2X XEON GOLD 6138.png";
 
 function EquipamentsGallery() {
   return (
@@ -13,26 +16,26 @@ function EquipamentsGallery() {
       <Container>
         <Cube $backgroundImage={desktops}>
           <CubeContent>
-            <Text>Desktops</Text>
-            <CubeImage src={desk} alt="Desk" />
+            <Text>Workstations</Text>
+            <CubeImage style={{width: "120px", height: "auto", marginLeft: "8px"}} src={work} alt="Workstation" />
           </CubeContent>
         </Cube>
         <Cube $backgroundImage={monitors}>
           <CubeContent>
             <Text>Monitores</Text>
-            <CubeImage src={desk} alt="Desk" />
+            <CubeImage style={{width: "140px", height: "140px", marginLeft: "8px"}} src={screen} alt="Screen" />
           </CubeContent>
         </Cube>
         <Cube $backgroundImage={notebooks}>
           <CubeContent>
             <Text>Notebooks</Text>
-            <CubeImage src={desk} alt="Desk" />
+            <CubeImage style={{width: "120px", height: "auto", marginLeft: "8px"}} src={note} alt="Notebook" />
           </CubeContent>
         </Cube>
         <Cube $backgroundImage={servers}>
           <CubeContent>
             <Text>Servidores</Text>
-            <CubeImage src={desk} alt="Desk" />
+            <CubeImage style={{width: "120px", height: "auto", marginLeft: "8px"}} src={server} alt="Server" />
           </CubeContent>
         </Cube>
       </Container>
