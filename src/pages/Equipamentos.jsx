38de096e378556308f5a75eba2b46d791c9d70e4
lucasmarcosds <lukeskaiw@gmail.com>
2026-04-24 14:52:00 -0300

    new equipamnetos

--- src/pages/Equipamentos.jsx
+++ src/pages/Equipamentos.jsx
@@ -7,73 +7,87 @@ import EquipamentoFilter from "../components/EquipamentoFilter";
 import EquipamentoCard from "../components/EquipamentoCard";
 
 import desktop from "../assets/mock-produto.png";
-import servidor from "../assets/servidor.png";
-import notebook from "../assets/notebook.png";
-import workstation from '../assets/workstation.png'
+import minidesk from "../assets/equipamentos-2026/DESK MINI DELL OPTIPLEX 3000 I3-12100T.jpg"
+import desktop2 from "../assets/equipamentos-2026/DESKTOP DELL SFF OPTIPLEX 3080 I5-10500.webp"
+import zbook from "../assets/equipamentos-2026/HpZbookFury15G7-side22_1382x.webp"
+import elitebook from "../assets/equipamentos-2026/NOTEBOOK HP ELITEBOOK 840 G7 I5-10310U.png"
+import zbook2 from "../assets/equipamentos-2026/NOTEBOOK WORKSTATION HP ZBOOK FURY 15 G7 I7-10850H 1.webp"
+import poweredge from "../assets/equipamentos-2026/SERVIDOR DELL POWEREDGE R540 2X XEON GOLD 6138 1.webp" 
+import poweredge2 from "../assets/equipamentos-2026/SERVIDOR DELL POWEREDGE T440 2X XEON GOLD 6138.png"
+import thinksystem from "../assets/equipamentos-2026/SERVIDOR LENOVO THINKSYSTEM SR630 2X XEON GOLD 6138 1.png";
+import z2tower from "../assets/equipamentos-2026/WORKSTATION HP Z2 G4 TOWER XEON E-2144G.webp";
 import EquipamentoBannerFooter from "../components/EquipamentoBannerFooter";
 import bgImage from "../assets/retangle-banner.png";
 
 const mockItem = [
   {
-    name: "Computador A620 i3",
-    image: desktop,
+    name: "OPTIPLEX 3000 I3-12100T",
+    image: minidesk,
+    category: "Minidesks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
+    gallery: [desktop, desktop, desktop, desktop],
+  },
+  {
+    name: "SFF OPTIPLEX 3080 I5-10500",
+    image: desktop2,
     category: "Desktop",
     description:
       "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
     gallery: [desktop, desktop, desktop, desktop],
   },
   {
-    name: "Computador A620 i3",
-    image: desktop,
+    name: "ZBOOK FURY 15 G7 I7-10850H",
+    image: zbook,
+    category: "Notebooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
+    gallery: [desktop, desktop, desktop, desktop],
+  },
+  {
+    name: "ELITEBOOK 840 G7 I5-10310U",
+    image: elitebook,
+    category: "Notebooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
+    gallery: [desktop, desktop, desktop, desktop],
+  },
+  {
+    name: "ZBOOK FURY 15 G7 I7-10850H",
+    image: zbook2,
+    category: "Notebooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
+    gallery: [desktop, desktop, desktop, desktop],
+  },
+  {
+    name: "POWEREDGE R540 2X XEON GOLD 6138",
+    image: poweredge,
     category: "Servidores",
     description:
       "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
     gallery: [desktop, desktop, desktop, desktop],
   },
   {
-    name: "Computador A620 i3",
-    image: desktop,
-    category: "Desktop",
+    name: "POWEREDGE T440 2X XEON GOLD 6138",
+    image: poweredge2,
+    category: "Servidores",
     description:
       "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
     gallery: [desktop, desktop, desktop, desktop],
   },
   {
-    name: "Computador A620 i3",
-    image: desktop,
-    category: "Notebooks",
+    name: "THINKSYSTEM SR630 2X XEON GOLD 6138",
+    image: thinksystem,
+    category: "Servidores",
     description:
       "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
     gallery: [desktop, desktop, desktop, desktop],
   },
   {
-    name: "Computador A620 i3",
-    image: desktop,
-    category: "Notebooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
-    gallery: [desktop, desktop, desktop, desktop],
-  },
-  {
-    name: "Computador A620 i3",
-    image: desktop,
-    category: "Notebooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
-    gallery: [desktop, desktop, desktop, desktop],
-  },
-  {
-    name: "Computador A620 i3",
-    image: desktop,
-    category: "Notebooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
-    gallery: [desktop, desktop, desktop, desktop],
-  },
-  {
-    name: "Computador A620 i3",
-    image: desktop,
-    category: "Desktop",
+    name: "Z2 G4 TOWER XEON E-2144G",
+    image: z2tower,
+    category: "Workstation",
     description:
       "Lorem ipsum dolor sit amet consectetur. Leo donec netus libero facilisi.",
     gallery: [desktop, desktop, desktop, desktop],
