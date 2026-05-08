857d8ee38b913067e14363ffcdd0d0814b69a154
ThalesAltarugio <166618351+Taizi0@users.noreply.github.com>
2026-05-08 08:29:01 -0300

    change EquipamentsGallery images

--- src/components/EquipamentsGallery/index.jsx
+++ src/components/EquipamentsGallery/index.jsx
@@ -5,10 +5,10 @@ import monitors from "../../assets/Gallery/screens.png";
 import notebooks from "../../assets/Gallery/notebooks.png";
 import servers from "../../assets/Gallery/servers.png";
 
-import work from "../../assets/equipamentos-2026/WORKSTATION HP Z2 G4 TOWER XEON E-2144G.webp";
-import screen from "../../assets/equipamentos-2026/1200.webp";
-import note from "../../assets/equipamentos-2026/NOTEBOOK WORKSTATION HP ZBOOK FURY 15 G7 I7-10850H 1.webp"
-import server from "../../assets/equipamentos-2026/SERVIDOR DELL POWEREDGE T440 2X XEON GOLD 6138.png";
+import work from "../../assets/Equipamentos-Site-2026/Workdesk/Sem-Título-1_0006_imgi_60_71awmxgow0L._AC_SL1413_.png";
+import screen from "../../assets/Equipamentos-Site-2026/Monitor/imgi_67_Monitor-Hp-Series-5-Pro-24-Fhd-Usb-c-Com-Webcam-Pop-up-5mp-udio-Com-Ia-100w-Power-Delivery-E-Design-Sustent-vel_1776256732.png";
+import note from "../../assets/Equipamentos-Site-2026/Note/imgi_7_Notebook-Hp-Elitebook-640-G11-Pot-ncia-Ia-Seguran-a-Corporativa-E-Mobilidade-Profissional-Tela-14-16-10-Desempenho-Intel-Para-Trabalho-H-brido_1776256735.png";
+import server from "../../assets/Equipamentos-Site-2026/Servidores/imgi_3_server-poweredge-t560-silver-gallery-5.png";
 
 function EquipamentsGallery() {
   return (
@@ -17,25 +17,25 @@ function EquipamentsGallery() {
         <Cube $backgroundImage={desktops}>
           <CubeContent>
             <Text>Workstations</Text>
-            <CubeImage style={{width: "110px", height: "auto", marginLeft: "8px"}} src={work} alt="Workstation" />
+            <CubeImage style={{width: "90px", height: "auto", marginLeft: "8px"}} src={work} alt="Workstation" />
           </CubeContent>
         </Cube>
         <Cube $backgroundImage={monitors}>
           <CubeContent>
             <Text>Monitores</Text>
-            <CubeImage style={{width: "120px", height: "auto", marginLeft: "8px"}} src={screen} alt="Screen" />
+            <CubeImage style={{width: "160px", height: "auto", marginLeft: "8px"}} src={screen} alt="Screen" />
           </CubeContent>
         </Cube>
         <Cube $backgroundImage={notebooks}>
           <CubeContent>
             <Text>Notebooks</Text>
-            <CubeImage style={{width: "120px", height: "auto", marginLeft: "8px"}} src={note} alt="Notebook" />
+            <CubeImage style={{width: "155px", height: "auto", marginLeft: "8px"}} src={note} alt="Notebook" />
           </CubeContent>
         </Cube>
         <Cube $backgroundImage={servers}>
           <CubeContent>
             <Text>Servidores</Text>
-            <CubeImage style={{width: "100px", height: "auto", marginLeft: "8px"}} src={server} alt="Server" />
+            <CubeImage style={{width: "120px", height: "auto", marginLeft: "8px"}} src={server} alt="Server" />
           </CubeContent>
         </Cube>
       </Container>
